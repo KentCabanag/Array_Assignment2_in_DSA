21aa0ca65ad6f7e360666ac96310beeb52c3cf25
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-11-05 12:59:32 +0800

    Last commit

--- Assignment2.py
+++ Assignment2.py
@@ -29,18 +29,18 @@
 #The element has been deleted
 #This is the new array: Array: [1, 4, 3, 4, 5, 6 ,2 ,56, 200]
 Array = [6, 4, 2, 8, 10, 18, 14, 16, 12, 20]
-print("Index Array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]")
-print(f"      Array: {Array}")
-print("Menu:")
-print("Pick A number on this")
-print("1 -> Add an element")
-print("2 -> Insert an element")
-print("3 -> Modify an element")
-print("4 -> Delete an elementt")
-print("5 -> Arrange in ascending order")
-print("6 -> Arrange in descending order")
-print()
-Question = int(input("What do you want to do? "))
+print("\n\033[32;1mIndex Array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]\033[0m ")
+print(f"      \033[32;1mArray: {Array}\033[0m")
+print("\n\033[34;4mMenu:\033[0m")
+print("\033[31;1mPick A number on this\033[0m")
+print("\033[34;4m1\033[0m -> Add an element")
+print("\033[34;4m2\033[0m -> Insert an element")
+print("\033[34;4m3\033[0m -> Modify an element")
+print("\033[34;4m4\033[0m -> Delete an elementt")
+print("\033[34;4m5\033[0m -> Arrange in ascending order")
+print("\033[34;4m6\033[0m -> Arrange in descending order\n")
+
+Question = int(input("\033[33;1mWhat do you want to do\033[0m ? "))
 def num1():
     if Question == 1:
         Type_num = int(input("Type the Number that you want to add: "))
@@ -52,7 +52,7 @@ def num1():
         Type_num2 = int(input("Type the Number that you want to insert: "))
         Type_num3 = int(input("Where do you want to insert? "))
         Add1 = Array.insert(Type_num3, Type_num2)
-        print("The element has been Insert")
+        print("\033[36;1mThe element has been Insert\033[0m")
         return Add1
     
     elif Question == 3:
@@ -60,26 +60,25 @@ def num1():
         Type_num5 = int(input("Type the Number that you want to add: "))
         Add2 = Array.pop(Type_num4)
         Array.insert(Type_num4, Type_num5)
-        print("The element has been Modify")
-
+        print("\033[36;1mThe element has been Modify\033[0m")
         return Add2
 
     elif Question == 4:
-        Type_5 = int(input("Type the Number that you want to Delete: "))
+        Type_5 = int(input("\033[36;1mType the Number that you want to Delete\033[0m: "))
         Add3 = Array.remove(Type_5)
         return Add3
 
     elif Question == 5:
         Add4 = Array.sort()
-        print("The element has been arrange to ascending order")
+        print("\033[36;1mThe element has been arrange to ascending order\033[0m")
         return Add4
 
     elif Question == 6:
         Add5 = Array.reverse()
-        print("The element has been arrange to descending order")
+        print("\033[36;1mTThe element has been arrange to descending order\033[0m")
         return Add5
 
 
 num1()
 print()
-print(f"This is the new array: Array : {Array}")
+print(f"\033[37;1mThis is the new array:\033[0m \033[31;1mArray\033[0m : \033[32;1m{Array}\033[0m")
